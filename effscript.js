d2b263b0824002b86e51bfe1ab29d50a63d5083d
Jacob Hartman <Jacob Hartman>
2016-03-15 20:53:17 -0400

    addition of jQuery assignment

---
+++ effscript.js
@@ -0,0 +1,29 @@
+function showAll(){
+	$(document).ready(function(){
+		$("div").show();
+	})
+}
+
+function hideAll(){
+	$(document).ready(function(){
+		$("div").hide();
+	})
+}
+
+function fadeSwitch(){
+	$(document).ready(function(){
+		$("div").fadeToggle(500);
+	})
+}
+
+function slideSwitch(){
+	$(document).ready(function(){
+		$("div").slideToggle(500);
+	})
+}
+
+function displaySwitch(){
+	$(document).ready(function(){
+		$("div").toggle(500);
+	})
+}
